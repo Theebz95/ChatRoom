f5dd0b7a5fad2a39e8af0d02692c6ac0a80a3d64
Kenny Nguyen <kenny>
2019-06-05 19:55:50 -0400

    init

---
+++ ChatroomBE/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import socketio
+# from wsgi import app
+  # a Flask, Django, etc. application
+appF = Flask(__name__)
+
+# create a Socket.IO server
+sio = socketio.Server()
+
+app = socketio.WSGIApp(sio, appF)
+
+
+@sio.event
+def connect(sid, environ):
+    print('connect ', sid)
+
+@sio.event
+async def chat_message(sid, data):
+    print("message ", data)
+    await sio.emit('reply', room=sid)
+
+@sio.event
+def disconnect(sid):
+    print('disconnect ', sid)
+    
